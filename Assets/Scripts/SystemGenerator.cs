f8957669d5b7b20c233ef1e962d81842ef74aa47
Roman Savchenko <savhenko14@gmail.com>
2021-02-25 09:53:36 +0200

    dev 25/02/2021 09:52
    + add mapsystem scene
    + add systemgenerator script

---
+++ Assets/Scripts/SystemGenerator.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SystemGenerator : MonoBehaviour
+{
+    public GameObject[] starPrefabs;
+    public GameObject[] planetPrefabs;
+    private void Awake()
+    {
+        
+    }
+}
