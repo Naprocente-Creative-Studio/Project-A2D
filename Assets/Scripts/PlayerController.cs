d7748d30aed138d73205fca5e6f59d0817a91ce8
Roman <63815847+ShacherMacher@users.noreply.github.com>
2021-07-24 22:56:51 +0300

    dev 24/07/2021 22:56
    OBT 3.3

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,6 +7,14 @@ public class PlayerController : MonoBehaviour
 	{
         playController = GameObject.Find("GameController").GetComponent<GamePlayController>();
 	}
+
+	private void FixedUpdate()
+	{
+        if (gameObject.transform.position.y < 0) gameObject.transform.position = new Vector3(gameObject.transform.position.x, 0, gameObject.transform.position.z);
+        if (gameObject.transform.position.y > 10) gameObject.transform.position = new Vector3(gameObject.transform.position.x, 10, gameObject.transform.position.z);
+        if (gameObject.transform.position.x < -2.5f) gameObject.transform.position = new Vector3(-2.5f, gameObject.transform.position.y, gameObject.transform.position.z);
+        if (gameObject.transform.position.x > 2.5f) gameObject.transform.position = new Vector3(2.5f, gameObject.transform.position.y, gameObject.transform.position.z);
+    }
 	private void OnCollisionEnter2D(Collision2D other)
     {
         if (other.gameObject.CompareTag("Comet"))
